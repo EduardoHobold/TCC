60b0cbfc6ec24d52c2bc9fe554b58f90d5d99a12
Eduardo Hobold <hobold.edu@hotmail.com>
2022-04-19 22:18:15 -0300

    Implementação dos icons na geração de itens/criando lógica para gerar itens da lista

--- src/pages/reflexividade/index.js
+++ src/pages/reflexividade/index.js
@@ -2,41 +2,74 @@ import React, { useState, useEffect, useCallback } from 'react';
 import { Text, View, FlatList, TouchableOpacity, Button } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import styles from './styles'
+import IconFont from 'react-native-vector-icons/FontAwesome5'
+import IconMaterial from 'react-native-vector-icons/MaterialCommunityIcons'
+
 
 export default function App() {
 
 	const data = [
-		{ id: 1, key: 'A' }, { id: 2, key: 'B' }, { id: 3, key: 'C' }, { id: 4, key: 'D' },
-		{ id: 5, key: 'E' }, { id: 6, key: 'F' }, { id: 7, key: 'G' }, { id: 8, key: 'H' }, { id: 9, key: 'I' }
+		{ id: 1, icon: 'dog', origem: 'FontAwesome5' }, { id: 2, icon: 'cat', origem: 'FontAwesome5' },
+		{ id: 3, icon: 'hippo', origem: 'FontAwesome5' }, { id: 4, icon: 'crow', origem: 'FontAwesome5' },
+		{ id: 5, icon: 'frog', origem: 'FontAwesome5' }, { id: 6, icon: 'fish', origem: 'FontAwesome5' },
+		{ id: 7, icon: 'horse', origem: 'FontAwesome5' }, { id: 8, icon: 'spider', origem: 'FontAwesome5' },
+		{ id: 9, icon: 'dragon', origem: 'FontAwesome5' }, { id: 10, icon: 'bat', origem: 'MaterialCommunityIcons' },
+		{ id: 11, icon: 'rabbit', origem: 'MaterialCommunityIcons' }, { id: 12, icon: 'pig-variant', origem: 'MaterialCommunityIcons' },
+		{ id: 13, icon: 'bird', origem: 'MaterialCommunityIcons' }, { id: 14, icon: 'dolphin', origem: 'MaterialCommunityIcons' },
+		{ id: 15, icon: 'bee', origem: 'MaterialCommunityIcons' }, { id: 16, icon: 'bee-flower', origem: 'MaterialCommunityIcons' },
+		{ id: 17, icon: 'snake', origem: 'MaterialCommunityIcons' }, { id: 18, icon: 'duck', origem: 'MaterialCommunityIcons' },
+
 	];
 
-	const [randon, setRandon] = useState(Math.floor(Math.random() * data.length - 1) + 1);
+	const [randon, setRandon] = useState(Math.floor(Math.random() * 9 - 1) + 1);
 	const [count, setCount] = useState(0);
 	const [press, setPress] = useState(null);
 
 	const [countSeconds, setCountSeconds] = useState(0);
 	const [customInterval, setCustomInterval] = useState();
 	const [result, setResult] = useState([])
+	const [loading, setLoading] = useState(true)
+	const [itens, setItens] = useState([])
 
 	useEffect(() => {
+		async function fetch() {
+			geraItens();
+			setLoading(false);
+		}
+		fetch();
+		setLoading(false)
 		validaResposta();
 	}, [press]);
+
+	function geraItens() {
+		console.log('geraItens')
+		let c = 0
+		for (var i = 0; i < data.length; i++) {
+			c = Math.floor(Math.random() * data.length - 1) + 1
+			if (c !== data.id && itens.length < 9) {
+				itens.push(data[c]);
+			}
+		}
+		setRandon(Math.floor(Math.random() * 9 - 1) + 1);
+		console.log(itens)
+		console.log(randon)
+	}
 
 	// Validação da resposta selecionada e monta array do obejto do resultado
 	function validaResposta() {
 		let resposta = false
 		let resultado
 		setCountSeconds((value) => value = 0);
-		if (press != null && data[randon].id === press.id) {
+		if (press != null && itens[randon].id === press.id) {
 			setCount(count + 1);
-			setRandon(Math.floor(Math.random() * data.length - 1) + 1);
+			setRandon(Math.floor(Math.random() * 9 - 1) + 1);
 			resposta = true
 		}
 		resultado = { id: result.length, acerto: resposta, tempo: countSeconds }
-		if(resultado){
+		if (resultado) {
 			result.push(resultado)
 		}
-		console.log(resposta, countSeconds)
+		// console.log(resposta, countSeconds)
 		console.log(result)
 	}
 
@@ -44,59 +77,61 @@ export default function App() {
 	const startTimer = () => {
 		setCustomInterval(
 			setInterval(() => {
-				changeTime();
+				setCountSeconds((value) => value + 1)
 			}, 1000)
 		)
 	}
 
-	const changeTime = () => {
-		setCountSeconds((value) => value + 1)
-	}
-
 	const stopTimer = () => {
-		if(customInterval) {
+		if (customInterval) {
 			clearInterval(customInterval)
 		}
 	}
 
-
-	
-
 	const renderItem = ({ item }) => {
 		return (
 			<TouchableOpacity onPress={() => setPress(item)} style={styles.item}>
-				<Text style={styles.itemText}>{item.key}</Text>
+				{item.origem == 'FontAwesome5' ? <IconFont name={item.icon} size={40} color="#fff" /> : <IconMaterial name={item.icon} size={40} color="#fff" />}
 			</TouchableOpacity>
 		);
 
 	};
 
-	return (
-		<LinearGradient colors={['rgba(25,38,68,1)', 'rgba(54,84,168,1)']} style={styles.container} >
-			<View style={{ alignItems: 'center', marginBottom: 20 }}>
-				<Text style={styles.itemText}>Comparação Matemática</Text>
-				<Text style={styles.itemText}>Acertos: {count.toString()}</Text>
-				<Text style={styles.itemText}>{countSeconds < 10 ? "0" + countSeconds : countSeconds}</Text>
-				<View style={{width: '50%', flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center'}}>
-					<Button title='Start' onPress={startTimer} />
-					<Button title='Stop' onPress={stopTimer} />
-				</View>	
-			</View>
+	if (!loading) {
+		return (
+			<LinearGradient colors={['rgba(25,38,68,1)', 'rgba(54,84,168,1)']} style={styles.container} >
+				<View style={{ alignItems: 'center', marginBottom: 20 }}>
+					<Text style={styles.itemText}>Comparação Matemática</Text>
+					<Text style={styles.itemText}>Acertos: {count.toString()}</Text>
+					<Text style={styles.itemText}>{countSeconds < 10 ? "0" + countSeconds : countSeconds}</Text>
+					<View style={{ width: '50%', flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center' }}>
+						<Button title='Start' onPress={startTimer} />
+						<Button title='Stop' onPress={stopTimer} />
+					</View>
+				</View>
 
-			<View style={[styles.button]}>
-				<Text style={styles.itemText}>{data[randon].key}</Text>
-			</View>
+				<View style={[styles.button]}>
+					{itens[randon].origem == 'FontAwesome5' ? <IconFont name={itens[randon].icon} size={40} color="#fff" /> : <IconMaterial name={itens[randon].icon} size={40} color="#fff" />}
+					{/* <IconFont name={itens[randon].icon} size={40} color="#fff" /> */}
+				</View>
 
-			<View style={{margin: 8}}> 
-				<FlatList
-					data={data}
-					renderItem={renderItem}
-					keyExtractor={(item) => item.id}
-					numColumns={3}
-				/>
-			</View>
+				<View style={{ margin: 8 }}>
+					<FlatList
+						data={itens}
+						renderItem={renderItem}
+						keyExtractor={(item) => item.id}
+						numColumns={3}
+					/>
+				</View>
+			</LinearGradient>
+		);
 
-		</LinearGradient>
-	);
+	} else {
+		return (
+			<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+				<Text> Carregando! </Text>
+			</View>
+		);
+	}
 
 }
